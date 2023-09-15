3d1561490d4c499dd52c0c6b9a4c4135125aceec
HrishikShaji <hrishikminishaji@gmail.com>
2023-09-15 11:14:11 +0530

    sidebar done

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      <p>Dashboard</p>
     </div>
   );
 };
---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./Sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button className="md:hidden" variant="ghost" size="icon">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
